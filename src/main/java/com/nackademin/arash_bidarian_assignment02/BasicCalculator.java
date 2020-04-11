3dc45e0fb7653270f64d39d27195d662716aa15b
Arash Bidarian <arash.bquest@gmail.com>
2020-04-11 12:39:23 +0200

    First commit of UTW05 Assignment 02

---
+++ src/main/java/com/nackademin/arash_bidarian_assignment02/BasicCalculator.java
@@ -0,0 +1,10 @@
+package com.nackademin.arash_bidarian_assignment02;
+
+public class BasicCalculator implements BasicOperations{
+
+	@Override
+	public double addition(double firstNumber, double secondNumber) {
+		return firstNumber + secondNumber;
+	}
+
+}
---
+++ src/main/java/com/nackademin/arash_bidarian_assignment02/BasicOperations.java
@@ -0,0 +1,12 @@
+package com.nackademin.arash_bidarian_assignment02;
+
+public interface BasicOperations {
+	public double addition(double firstNumber, double secondNumber);
+	
+
+//	Basic operation  
+//	System.out.println("addition, subtraction, multiplication, division" );
+	
+	
+
+}

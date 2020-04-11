aa895a2bc46046f393957b45c300fb9fffc62d29
Arash Bidarian <arash.bquest@gmail.com>
2020-04-11 22:52:36 +0200

    UTW05 Assignment 02: BasicOperations are added

--- src/main/java/com/nackademin/arash_bidarian_assignment02/BasicCalculator.java
+++ src/main/java/com/nackademin/arash_bidarian_assignment02/BasicCalculator.java
@@ -2,9 +2,20 @@ package com.nackademin.arash_bidarian_assignment02;
 
 public class BasicCalculator implements BasicOperations{
 
-	@Override
 	public double addition(double firstNumber, double secondNumber) {
 		return firstNumber + secondNumber;
+	}
+
+	public double substraction(double firstNumber, double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+
+	public double multipication(double firstNumber, double secondNumber) {
+		return firstNumber * secondNumber;	}
+
+	public double dicvision(double firstNumber, double secondNumber) {
+		return firstNumber / secondNumber;		
+		
 	}
 
 }
--- src/main/java/com/nackademin/arash_bidarian_assignment02/BasicOperations.java
+++ src/main/java/com/nackademin/arash_bidarian_assignment02/BasicOperations.java
@@ -2,8 +2,11 @@ package com.nackademin.arash_bidarian_assignment02;
 
 public interface BasicOperations {
 	public double addition(double firstNumber, double secondNumber);
-	
+	public double substraction(double firstNumber, double secondNumber);
+	public double multipication(double firstNumber, double secondNumber);
+	public double dicvision(double firstNumber, double secondNumber);
 
+	
 //	Basic operation  
 //	System.out.println("addition, subtraction, multiplication, division" );
 	

43a1b1718f968916e8de5665acf27768f00ab011
Arash Bidarian <arash.bquest@gmail.com>
2020-04-11 22:37:55 +0200

    UTW05 Assignment 02-Adding Advanced Interface & Advace Class

---
+++ src/main/java/com/nackademin/arash_bidarian_assignment02/AdvancedCalculator.java
@@ -0,0 +1,10 @@
+package com.nackademin.arash_bidarian_assignment02;
+
+public class AdvancedCalculator implements AdvancedOperations{
+	 double number= 0.0;  
+	@Override
+	public double absValue(double number) {
+		return Math.abs(number);
+	}
+
+}
---
+++ src/main/java/com/nackademin/arash_bidarian_assignment02/AdvancedOperations.java
@@ -0,0 +1,6 @@
+package com.nackademin.arash_bidarian_assignment02;
+
+public interface AdvancedOperations {
+	public double absValue(double number);
+	
+}

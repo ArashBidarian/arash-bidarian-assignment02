7548c4257b598a98d352b4ad2326b4696a96f241
Arash Bidarian <arash.bquest@gmail.com>
2020-04-13 13:36:01 +0200

    UTW05 Assignment 02: första test som fungerar inte riktig bra :-(

--- src/test/java/com/nackademin/arash_bidarian_assignment02/BasicCalculatorTest.java
+++ src/test/java/com/nackademin/arash_bidarian_assignment02/BasicCalculatorTest.java
@@ -9,8 +9,9 @@ public class BasicCalculatorTest {
 	
 
 	@Test
+	// assertEquals(initieringen.funktion/metod(värde1,värde2, värde att matcha, avvikelsetollerans))
 	public void test() {
-		assertEquals("bc.addition(2,1), 3.0"); 
+		assertEquals(bc.addition(2,1), 3, 0); 
 	}
 
 }

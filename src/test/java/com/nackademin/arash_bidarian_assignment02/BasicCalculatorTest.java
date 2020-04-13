697d50585128b879c8e2a550c9d4184584c3a2c7
Arash Bidarian <arash.bquest@gmail.com>
2020-04-13 17:18:30 +0200

    UTW05 Assignment 02: Randomtester för Basic Calculator inlagda

--- src/test/java/com/nackademin/arash_bidarian_assignment02/BasicCalculatorTest.java
+++ src/test/java/com/nackademin/arash_bidarian_assignment02/BasicCalculatorTest.java
@@ -2,16 +2,98 @@ package com.nackademin.arash_bidarian_assignment02;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+import java.util.logging.Logger;
+
 import org.junit.Test;
 
 public class BasicCalculatorTest {
-	BasicCalculator bc = new BasicCalculator();
-	
+	BasicCalculator bcTest = new BasicCalculator();
+	Random random = new Random();
+	public static final Logger LOG = Logger.getLogger(BasicCalculator.class.getName());
 
+	BasicCalculator bc = new BasicCalculator();
+
+//	Random Test for addition, subtraction, multiplication, division
+//==================================================================
+	
+	//	Random Test for addition
 	@Test
 	// assertEquals(initieringen.funktion/metod(värde1,värde2, värde att matcha, avvikelsetollerans))
-	public void test() {
-		assertEquals(bc.addition(2,1), 3, 0); 
+	public void randomTestAddition() {
+		double tempRandomValue1 = 0.0;
+		double tempRandomValue2 = 0.0;
+		double tempResult = 0.0;
+		for (int i = 0;  i < 5; i++) {
+			tempRandomValue1 = random.nextInt()/9.0;
+			tempRandomValue2 = random.nextInt()/9.0;
+			tempResult = tempRandomValue1 + tempRandomValue2; 
+			LOG.info("Testing the method addition wtih: " + tempRandomValue1 + " & " + tempRandomValue2);
+			assertEquals(bcTest.addition(tempRandomValue1, tempRandomValue2), tempResult, 0);
+		}
 	}
 
+	//	Random Test for subtraction
+	@Test
+	// assertEquals(initieringen.funktion/metod(värde1,värde2, värde att matcha, avvikelsetollerans))
+	public void randomTestSubtraction() {
+		double tempRandomValue1 = 0.0;
+		double tempRandomValue2 = 0.0;
+		double tempResult = 0.0;
+		for (int i = 0;  i < 5; i++) {
+			tempRandomValue1 = random.nextInt()/9.0;
+			tempRandomValue2 = random.nextInt()/9.0;
+			tempResult = tempRandomValue1 - tempRandomValue2; 
+			LOG.info("Testing the method addition wtih: " + tempRandomValue1 + " & " + tempRandomValue2);
+			assertEquals(bcTest.subtraction(tempRandomValue1, tempRandomValue2), tempResult, 0);
+		}
+	}
+
+	//	Random Test for  multiplication
+	@Test
+	// assertEquals(initieringen.funktion/metod(värde1,värde2, värde att matcha, avvikelsetollerans))
+	public void randomTestMultiplication() {
+		double tempRandomValue1 = 0.0;
+		double tempRandomValue2 = 0.0;
+		double tempResult = 0.0;
+		for (int i = 0;  i < 5; i++) {
+			tempRandomValue1 = random.nextInt()/9.0;
+			tempRandomValue2 = random.nextInt()/9.0;
+			tempResult = tempRandomValue1 * tempRandomValue2; 
+			LOG.info("Testing the method addition wtih: " + tempRandomValue1 + " & " + tempRandomValue2);
+			assertEquals(bcTest.multiplication(tempRandomValue1, tempRandomValue2), tempResult, 0);
+		}
+	}
+	
+	//	Random Test for division
+	
+	@Test
+	// assertEquals(initieringen.funktion/metod(värde1,värde2, värde att matcha, avvikelsetollerans))
+	public void randomTtestDivision() {
+		double tempRandomValue1 = 0.0;
+		double tempRandomValue2 = 0.0;
+		double tempResult = 0.0;
+		for (int i = 0;  i < 5; i++) {
+			tempRandomValue1 = random.nextInt()/9.0;
+			tempRandomValue2 = random.nextInt()/9.0;
+			tempResult = tempRandomValue1 / tempRandomValue2; 
+			LOG.info("Testing the method addition wtih: " + tempRandomValue1 + " & " + tempRandomValue2);
+			assertEquals(bcTest.division(tempRandomValue1, tempRandomValue2), tempResult, 0);		
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
